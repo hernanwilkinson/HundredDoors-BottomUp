824c3a94492655d4c791502d7ccb8e2c856027b6
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-08 13:50:12 -0300

    Test pass: doorEndsOpenedWhenFlippedFromClosed

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -1,9 +1,11 @@
 public class DoorsFlipper {
+    private boolean closed = true;
+
     public boolean isClosed(int i) {
-        return true;
+        return closed;
     }
 
     public void flip() {
-        throw new RuntimeException();
+        closed = false;
     }
 }
