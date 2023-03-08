5393faf42435fbae6998226fb24b376d9427ede7
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-08 14:15:02 -0300

    Refactor: Extract method assertValidNumberOfDoors

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -3,9 +3,13 @@ public class DoorsFlipper {
     private boolean opened[];
 
     public DoorsFlipper(int numberOfDoors) {
-        if (numberOfDoors<1) throw new IllegalArgumentException(INVALID_NUMBER_OF_DOORS);
+        assertValidNumberOfDoors(numberOfDoors);
 
         opened = new boolean[numberOfDoors];
+    }
+
+    private void assertValidNumberOfDoors(int numberOfDoors) {
+        if (numberOfDoors <1) throw new IllegalArgumentException(INVALID_NUMBER_OF_DOORS);
     }
 
     public boolean isClosed(int doorPosition) {
