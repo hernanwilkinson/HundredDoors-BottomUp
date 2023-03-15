edb3fc6ecfc415a93c8f58a919cbe66649c615c5
Hernán Wilkinson <hernan.wilkinson@10pines.com>
2023-03-15 16:18:45 -0300

    Rename doorPosition a doorIndex donde corresponde - Rename nombre del proyecto a BottomUp

--- src/DoorsFlipper.java
+++ src/DoorsFlipper.java
@@ -2,7 +2,7 @@ public class DoorsFlipper {
     public static final String INVALID_NUMBER_OF_DOORS = "Number of doors must be strictly positive";
     public static final String INVALID_FLIP_STEP = "Step must be between 1 and number of doors";
     public static final String INVALID_DOOR_POSITION = "Door position must be between 1 and number of doors";
-    private boolean opened[];
+    private boolean[] opened;
 
     public DoorsFlipper(int numberOfDoors) {
         assertValidNumberOfDoors(numberOfDoors);
@@ -25,16 +25,16 @@ public class DoorsFlipper {
     public void flipEvery(int step) {
         assertIsValidStep(step);
 
-        for (int doorPosition = step-1; doorPosition < opened.length; doorPosition+=step)
-            flipAt(doorPosition);
+        for (int doorIndex = step-1; doorIndex < opened.length; doorIndex+=step)
+            flipAt(doorIndex);
     }
 
-    private void flipAt(int doorPosition) {
-        opened[doorPosition] = !opened[doorPosition];
+    private void flipAt(int doorIndex) {
+        opened[doorIndex] = !opened[doorIndex];
     }
 
     private void assertIsValidStep(int step) {
-        if (step <1 || step >opened.length) throw new IllegalArgumentException(INVALID_FLIP_STEP);
+        if (step < 1 || step > opened.length) throw new IllegalArgumentException(INVALID_FLIP_STEP);
     }
 
     public void flipAll() {
